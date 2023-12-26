97388734a9308464136399e81db4e22fae9df055
SerhiiOberemchuk <135756668+SerhiiOberemchuk@users.noreply.github.com>
2023-12-26 20:58:14 +0000

    Deploying to gh-pages from @ SerhiiOberemchuk/goit-react-hw-05-movies@4c2bf0f67386102924b25c64ea4b700b035a65ad 🚀

---
+++ static/js/7.1f39e547.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[7],{7:function(e,n,t){t.r(n);var i=t(791),r=t(184),u=(0,i.lazy)((function(){return t.e(890).then(t.bind(t,890))}));n.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)(u,{})})}}}]);
+//# sourceMappingURL=7.1f39e547.chunk.js.map
